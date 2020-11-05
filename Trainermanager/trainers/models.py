83d62e4a3c743079db50d60eeef00fc1126494a6
knightSarai <knight.sarai.dev@gmail.com>
2020-11-05 22:18:00 +0200

    string represintaion

--- Trainermanager/trainers/models.py
+++ Trainermanager/trainers/models.py
@@ -5,4 +5,7 @@ class Trainer(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
     
