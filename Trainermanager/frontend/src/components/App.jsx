0c735af34e0d6956b3b3ead920d5e519ef6dce15
knightSarai <knight.sarai.dev@gmail.com>
2020-11-06 19:16:02 +0200

    add layout

--- Trainermanager/frontend/src/components/App.jsx
+++ Trainermanager/frontend/src/components/App.jsx
@@ -1,11 +1,14 @@
-import React, {Component} from 'react'
-
+import React, { Component } from 'react'
+import Layout from '../components/layout';
 export default class App extends Component {
     render() {
         return (
-            <div>
-                react App
-            </div>
+            <Layout>
+                <div>
+                    react App
+                </div>
+            </Layout>
+
         )
     }
 }
