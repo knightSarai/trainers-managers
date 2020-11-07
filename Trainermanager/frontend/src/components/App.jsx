7e3ea35b4be658cc016fe00625619f3da8dbf551
knightSarai <knight.sarai.dev@gmail.com>
2020-11-07 10:55:05 +0200

    add react Alert

--- Trainermanager/frontend/src/components/App.jsx
+++ Trainermanager/frontend/src/components/App.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import Layout from '../components/layout';
 import Dashboard from './Trainer';
+/**React Alerts */
+import {positions, Provider as AlertProvider} from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+/**Alert Options */
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center'
+}
 
 export default class App extends Component {
     render() {
         return (
-            <Layout>
-                <div className="container">
-                    <Dashboard />
-                </div>
-            </Layout>
-
+            <AlertProvider template={AlertTemplate} {...alertOptions}>
+                <Layout>
+                    <div className="container">
+                        <Dashboard />
+                    </div>
+                </Layout>
+            </AlertProvider>
         )
     }
 }
