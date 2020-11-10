2ff48f3f861ce1770b3e2cc895fca7c3fe258290
knightSarai <knight.sarai.dev@gmail.com>
2020-11-10 18:14:07 +0200

    add manager

--- Trainermanager/trainers/models.py
+++ Trainermanager/trainers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Trainer(models.Model):
+    manager = models.ForeignKey(User, related_name="trainers", on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
